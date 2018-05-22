e6b3c3364358fe9282c9024ee38f076a16100bc9
witeoutmysin <lorenzocovarrubiasjr@gmail.com>
2018-05-22 14:57:12 -0500

    practicing with Avi 05/21/2018

--- Rakefile
+++ Rakefile
@@ -1,5 +1,10 @@
 require_relative './config/enviroment'
 
+def reload!
+    load './lib'
+end
+
+
 task :console do
   Pry.start
 end
--- lib/newsletter.rb
+++ lib/newsletter.rb
@@ -1,5 +1,30 @@
-class Newsletter
-  attr_accessor :articles, :issue_number, :issue_date
+class InvalidType < StandardError; end
 
+class Newsletter
+  attr_accessor :issue_number, :issue_date
+  attr_reader :articles
+
+def initialize
+  @articles = []
+end
+
+def articles
+  @articles.dup.freeze
+end
+
+def add_article(article)
+  if !article.is_a?(Article)
+      raise InvalidType, "Must Be An Article"
+  else
+  @@article << article
+  end
+end
+
+
+def first_article_url
+  #articles is an array
+  #the first element in that array must respond to url
+  self.articles.first.url
+end
 
 end
